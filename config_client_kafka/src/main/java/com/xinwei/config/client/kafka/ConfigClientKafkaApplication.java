d9267e191d5359417f783cfcb679417ee6d5563f
chunhuiwang <vigoroso@126.com>
2017-12-07 15:13:46 +0800

    Add files via upload

---
+++ config_client_kafka/src/main/java/com/xinwei/config/client/kafka/ConfigClientKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.xinwei.config.client.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientKafkaApplication.class, args);
+	}
+}
